734e2a7ea3ece2f6352ee7d4d167c783187dda48
hossaki <caroline.okamura@gmail.com>
2024-12-14 18:56:49 +0000

    categorias

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import { View } from "react-native"
-
+import { router } from "expo-router"
 
 import { Steps } from "@/components/steps";
 import { Button } from "@/components/button";
@@ -11,10 +11,11 @@ export default function Index(){
         <Welcome />
         <Steps /> 
 
-        <Button>
+        <Button onPress={() => router.navigate("/home")}>
             <Button.Title>Começar</Button.Title>
         </Button>
         </View>
+        
         
         
     )
